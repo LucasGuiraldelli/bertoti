ed7f4dd9579c3a6c662e4d7c63c357324c172bfd
Lucas Guiraldelli <lucas.guiraldelli@outlook.com>
2022-09-27 01:06:01 +0000

    singleton commit 2

--- PadroesProjetos/Singleton/Singleton.java
+++ PadroesProjetos/Singleton/Singleton.java
@@ -1,26 +1,32 @@
-class Singleton {
-    // Static variable reference of single_instance
-    // of type Singleton
-    private static Singleton single_instance = null;
-  
-    // Declaring a variable of type String
-    public String s;
-  
-    // Constructor
-    // Here we will be creating private constructor
-    // restricted to this class itself
-    private Singleton()
-    {
-        s = "Hello I am a string part of Singleton class";
+class Database {
+    private static Database dbObject;
+ 
+    private Database() {      
     }
-  
-    // Static method
-    // Static method to create instance of Singleton class
-    public static Singleton getInstance()
-    {
-        if (single_instance == null)
-            single_instance = new Singleton();
-  
-        return single_instance;
+ 
+    public static Database getInstance() {
+ 
+       // create object if it's not already created
+       if(dbObject == null) {
+          dbObject = new Database();
+       }
+ 
+        // returns the singleton object
+        return dbObject;
     }
-}
+ 
+    public void getConnection() {
+        System.out.println("You are now connected to the database.");
+    }
+ }
+ 
+ class Main {
+    public static void main(String[] args) {
+       Database db1;
+ 
+       // refers to the only object of Database
+       db1= Database.getInstance();
+       
+       db1.getConnection();
+    }
+ }
